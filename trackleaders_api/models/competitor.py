f632d696a18e204cbdc05a9ef2685005e3a52772
David Richey <davidrichey84@gmail.com>
2024-11-04 23:14:12 -0600

    racers parsed

--- trackleaders_api/models/competitor.py
+++ trackleaders_api/models/competitor.py
@@ -5,25 +5,24 @@ from typing import List, Dict, Optional
 
 class Competitor(BaseModel):
     competitor_name: str
-    competitor_from: Optional[str]
+    competitor_from: Optional[str] = None
     # update this to route model
     #competitor_route: Route
     # update this to route model
-    competitor_format: str
-    competitor_age: int
-    competitor_equipment: dict
+    competitor_age: Optional[int] = None
+    competitor_equipment: Optional[dict] = None
     competitor_status: str
-    competitor_speed_units: str
-    competitor_elevation_units: str
-    competitor_current_speed: float
-    competitor_moving_average_speed: float
-    competitor_last_report: str
-    competitor_time_since_start: str
-    competitor_route_mile: float
-    competitor_elevation_gain: int
-    competitor_current_elevation: int
-    competitor_route_distance_per_day: int
-    competitor_moving_time: str
-    competitor_stopped_time: str
-    competitor_next_waypoint: str
-    competitor_distance_to_next_waypoint: float
+    competitor_speed_units: Optional[str] = None
+    competitor_elevation_units: Optional[str] = None
+    competitor_current_speed: Optional[float] = None
+    competitor_moving_average_speed: Optional[float] = None
+    competitor_last_report: Optional[str] = None
+    competitor_time_since_start: Optional[str] = None
+    competitor_route_mile: Optional[str] = None
+    competitor_elevation_gain: Optional[int] = None
+    competitor_current_elevation: Optional[int] = None
+    competitor_route_distance_per_day: Optional[int] = None
+    competitor_moving_time: Optional[str] = None
+    competitor_stopped_time: Optional[str] = None
+    competitor_next_waypoint: Optional[str] = None
+    competitor_distance_to_next_waypoint: Optional[float] = None
--- trackleaders_api/models/race.py
+++ trackleaders_api/models/race.py
@@ -15,13 +15,66 @@ class Race(BaseModel):
     race_news: Optional[list] = None
     race_active: bool
     race_routes: Optional[List[Route]] = []
-    race_competitors: Optional[List[Competitor]] = None
+    race_competitors: Optional[List[Competitor]] = []
 
     def get_competitors(self):
         if self.race_routes is not None:
             print("getting racers")
         else:
             print("getting race routes first")
+
+    def _get_race_competitors(self, route=None):
+        temp_race_name = self.race_link.split("/")[3]
+        page = self.get_page(f"http://trackleaders.com/spot/{temp_race_name}/summary.php")
+        if page:
+            initial_soup = BeautifulSoup(page.content, "html.parser")
+            racer_table = initial_soup.find('table')
+            rows = racer_table.find_all('tr')
+            competitor_names = []
+            for row in rows:
+                # Extract all cells (td or th) in the row
+                cells = row.find_all('td')
+                if len(cells) > 0:
+                    competitor_names.append(cells[0].get_text(strip=True))
+            
+            for competitor in competitor_names:
+                competitor_name_link = re.sub(r'[ !@#$%^&*]', '_', competitor)
+                competitor_page = self.get_page(f"{self.race_link}i.php?name={competitor_name_link}")
+                if competitor_page:
+                    competitor_soup = BeautifulSoup(competitor_page.content, "html.parser")
+                    table = competitor_soup.find('table', id='statustable')
+                    table_rows = table.find_all('tr')
+                    competitor_dict = {'competitor_name': competitor}
+                    for row in table_rows:
+                        cells = row.find_all('td')
+                        cell_texts = [cell.get_text(strip=True) for cell in cells]
+                        if len(cell_texts) > 0:
+                            if cell_texts[0].lower() == 'race status':
+                                competitor_dict['competitor_status'] =cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'status':
+                                competitor_dict['competitor_status'] =cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == "last ppdate rec'd":
+                                pass
+                            elif cell_texts[0].lower() == 'current speed':
+                                competitor_dict['competitor_current_speed'] = cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'route mile':
+                                competitor_dict['competitor_route_mile'] = cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'elevation gain':
+                                competitor_dict['competitor_elevation_gain'] = cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'current elevation':
+                                competitor_dict['competitor_current_elevation'] = cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'moving time':
+                                competitor_dict['competitor_moving_time'] = cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'stopped time':
+                                competitor_dict['competitor_stopped_time'] = cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'moving average speed':
+                                competitor_dict['competitor_moving_average_speed'] = cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'next waypoint':
+                                competitor_dict['competitor_next_waypoint'] = cell_texts[1].split(' ')[0]
+                            elif cell_texts[0].lower() == 'distance to next waypoint':
+                                competitor_dict['competitor_distance_to_next_waypoint'] = cell_texts[1].split(' ')[0]
+                    print(competitor_dict)
+                    self.race_competitors.append(Competitor.model_validate(competitor_dict))
 
     def get_routes(self):
         page = self.get_page(f"{self.race_link}f.php")
@@ -38,14 +91,13 @@ class Race(BaseModel):
             for route in routes:
                 self.race_routes.append(Route(route_name=route[0], route_distance=route[1]))
 
-    def get_competitors(self):
-        temp_race_name = self.race_link.split("/")[3]
-        page = self.get_page(f"http://trackleaders.com/spot/{temp_race_name}/summary.php")
-        if page:
-            print("got the page... gets parse some racers!")
-            soup = BeautifulSoup(page.content, "html.parser")
-            racer_table = soup.find('table')
-
+    #def get_competitors(self):
+        #temp_race_name = self.race_link.split("/")[3]
+        #page = self.get_page(f"http://trackleaders.com/spot/{temp_race_name}/summary.php")
+        #if page:
+        #    print("got the page... gets parse some racers!")
+        #    soup = BeautifulSoup(page.content, "html.parser")
+        #    racer_table = soup.find('table')
 
     def get_page(self, race_link):
         try:
